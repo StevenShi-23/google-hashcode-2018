0469ff258c0f0a42c23c14c3e5f12baf5a8b2c78
Ziji Shi <shi.ziji.sm@gmail.com>
2018-03-02 05:05:20 +0800

    Data uploaded.debugged

---
+++ solution.py
@@ -0,0 +1,114 @@
+import math
+#from queue import PriorityQueue
+import heapq
+
+def dist_manh(start, end):
+    return abs(start[0]-end[0]) + abs(start[1]-end[1])
+
+
+class PriorityQueue(object):
+    def __init__(self):
+        self.heap = []
+
+    def add(self, d, pri):
+        heapq.heappush(self.heap, (pri, d))
+
+    def getSmallest(self):
+        pri, d = heapq.heappop(self.heap)
+        return d
+
+    def getLargest(self):
+        return self.heap.nlargest(1)[0]
+
+
+class Vehicle:
+    def __init__(self, position=(0, 0), status=0):
+        # free = 0, tostart = 1, todest = 2
+        self.position = position
+        self.rideList = []
+        self.status = status
+
+    def update(self,ride):
+        if ride.start_position[0] - self.position[0] > 0:
+            self.position[0] += 1
+        elif ride.start_position[0] - self.position[0] < 0:
+            self.position[0] -= 1
+        elif ride.start_position[1] - self.position[1] > 0:
+            self.position[1] += 1
+        elif ride.start_position[1] - self.position[1] < 0:
+            self.position[1] -= 1
+
+
+class Ride:
+    def __init__(self,
+                 id,
+                 start_position,
+                 end_position,
+                 earliest_start_time,
+                 latest_finish_time):
+        self.id = id
+        self.start_position = start_position
+        self.end_position = end_position
+        self.earliest_start_time = earliest_start_time
+        self.latest_finish_time = latest_finish_time
+        self.dist = abs(start_position[0] - end_position[0]) + abs(start_position[1] - end_position[1])
+
+    # lower the better
+    def cost(self, vehicle, current_time, bonus):
+        if not vehicle.available:
+            return math.inf
+
+        if current_time + dist_manh(vehicle.position, self.start_position) > self.latest_finish_time - self.dist:
+            return math.inf
+
+        return dist
+        # todo: include B
+
+
+class World:
+    def __init__(self, vehicles, rides, bonus, rows, cols, T):
+        self.vehicles = vehicles
+        # rides is a priority queue of things
+
+        self.rides = PriorityQueue()
+        for ride in rides:
+            self.rides.add(ride, ride.earliest_start_time)
+        self.current_time = 0
+        self.bonus = bonus
+        self.numRows = rows
+        self.numCols = cols
+        self.maxT = T
+
+    def next_step(self):
+
+        for vehicle in self.vehicles:
+
+            if vehicle.status == 0:
+                continue
+            else:
+                vehicle.update()
+
+    def run(self):
+        for t in range(0, self.maxT):
+            cur_ride = self.rides.getSmallest()
+            while (cur_ride.earliest_start_time == t):
+                temp_vehicle_PQ = PriorityQueue()
+                for vehicle in self.vehicles:
+                    if vehicle.status == 0:
+                        score = cur_ride.score(vehicle, t, self.bonus)
+                        temp_vehicle_PQ.add(vehicle, score)
+                selectedVehicle = temp_vehicle_PQ.getLargest()
+                selectedVehicle.rideList.append(cur_ride.id)
+                selectedVehicle.status = 1
+                cur_ride = self.rides.getSmallest()
+            self.next_step()
+
+    def reader(inputPath):
+        with open
+
+    def writer(self):
+
+
+
+
+
